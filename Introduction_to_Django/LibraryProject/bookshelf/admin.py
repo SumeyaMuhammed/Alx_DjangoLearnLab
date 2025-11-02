daadb8829fbe4f57577cb444fd3182164e863e79
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-02 08:47:33 -1100

    simplify the code and add search fields

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from .models import Book
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
-
-class BookAdmin(admin.ModelAdmin):
-    list_filter = ('title', 'author', 'publication_year')
-
-admin.site.register(Book, BookAdmin)
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
 
 
