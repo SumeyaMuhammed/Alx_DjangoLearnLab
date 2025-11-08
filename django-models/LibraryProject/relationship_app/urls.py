8ea29cd8c0313853eb6b3f1ee1ecd259cf88e71b
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-08 07:33:19 -1100

    refine method call

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,9 +3,9 @@ from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 
 urlpatterns = [
-    path('books/', list_books, name='book-list'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path("signup/", views.register.as_view(), name="signup"),
+    path('books/', views.list_books, name='book-list'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
+    path("signup/", views.register, name="signup"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
