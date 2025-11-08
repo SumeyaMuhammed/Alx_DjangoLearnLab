26178212c0a6c7254cf08c6deff7802b85db30d0
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-08 07:37:13 -1100

    refime import to align with the checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView, LoginView
 from . import views
+from views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', views.list_books, name='book-list'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
+    path('books/', list_books, name='book-list'),
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
     path("signup/", views.register, name="signup"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
