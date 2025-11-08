c9eb29185a468757883c5fe95aaa542d1dc2bb17
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-08 06:58:18 -1100

    change import method

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LogoutView, LoginView
+import views
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path("signup/", register.as_view(), name="signup"),
+    path("signup/", views.register.as_view(), name="signup"),
     path('login/', LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
 ]
