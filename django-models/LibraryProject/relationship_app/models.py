82617d7bab8fe19f4e23957b2579f231212ce62c
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-08 10:56:47 -1100

    Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 class Author(models.Model):
   name = models.CharField(max_length=200)
@@ -26,3 +29,14 @@ class Librarian(models.Model):
 
   def __str__(self):
     return self.name
+  
+class userProfile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, name='user')
+  role = models.CharField(max_length=200, choices=[('Admin','Admin'), 
+                                                   ('Librarian','Librarian'), 
+                                                   ('Member','Member')])
+  
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 from .views import list_books, LibraryDetailView
+from .admin_view import admin_dashboard
+from .member_view import member_dashboard
+from librarian_view import librarian_dashboard
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),
@@ -9,4 +12,7 @@ urlpatterns = [
     path("signup/", views.register, name="signup"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('admin/',  admin_dashboard, name="member-dashboard"),
+    path('member/',  librarian_dashboard, name="member-dashboard"),
+    path('member/', member_dashboard, name='member-dashboard'),
 ]
