183353bb5e467b174751be1bd66b7b8b92f5cb79
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-09 06:55:30 -1100

    change path name

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('admin/',  admin_dashboard, name="member-dashboard"),
     path('member/',  librarian_dashboard, name="member-dashboard"),
     path('member/', member_dashboard, name='member-dashboard'),
-    path('addbook',add_book, name='add-book' ),
-    path('changebook/<int:book_id>/',change_book, name='change-book' ),
-    path('deletebook/<int:book_id>/',delete_book, name='delete-book' ),
+    path('add_book',add_book, name='add-book' ),
+    path('edit_book/<int:book_id>/',change_book, name='change-book' ),
+    path('delete_book/<int:book_id>/',delete_book, name='delete-book' ),
 ]
