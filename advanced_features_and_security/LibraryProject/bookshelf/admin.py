7d2c7a71b17833ca4c11b6fac58941e545096df2
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-15 19:20:11 +0300

    Integrate the Custom User Model into Admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 from .models import Book
-
+from django.utils.translation import gettext_lazy as _
+from .models import Book, CustomUser
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
 
+class userAdmin(admin.ModelAdmin):
+  model = CustomUser
+  list_display = ("email", "date_of_birth", "profile_photo", "is_staff", "is_superuser", "is_active")
+  list_filter = ("is_staff", "is_superuser")
+  fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("date_of_birth", "profile_photo")}),
+        (_("Permissions"), {
+            "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions"),
+        }),
+    )
+
+  add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "date_of_birth", "profile_photo", "password1", "password2"),
+        }),
+    )
 
