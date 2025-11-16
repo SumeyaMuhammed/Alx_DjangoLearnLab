d9e2a3edfc30c540aeef2183fbfee4f2d8b999ea
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-16 19:15:37 +0300

    Create and Configure Groups with Assigned Permissions

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Book
 from django.utils.translation import gettext_lazy as _
 from .models import Book, CustomUser
+from django.contrib.auth.models import Group, Permission
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
@@ -26,5 +28,20 @@ class CustomUserAdmin(admin.ModelAdmin):
             "fields": ("email", "date_of_birth", "profile_photo", "password1", "password2"),
         }),
     )
+  
+def setup_groups():
+    perms = Permission.objects.filter(codename__in=[
+        "can_view", "can_create", "can_edit", "can_delete"
+    ])
 
-admin.site.register(CustomUser, CustomUserAdmin)"
+    editors = Group.objects.create(name="Editors")
+    editors.permissions.set(perms.filter(codename__in=["can_view", "can_edit", "can_create"]))
+
+    viewers = Group.objects.create(name="Viewers")
+    viewers.permissions.set(perms.filter(codename="can_view"))
+
+    admins = Group.objects.create(name="Admins")
+    admins.permissions.set(perms)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
