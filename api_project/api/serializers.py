b9ef4bc0048b0cc260462fcc962dc87794e91bce
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-21 10:49:03 +0300

    Build API Endpoint

---
+++ api_project/api/serializers.py
@@ -0,0 +1,7 @@
+import rest_framework
+from .models import Book
+
+class BookSerializer(rest_framework.serializers.ModelSerializer):
+  class Meta:
+    model = Book
+    fields = ['title', 'author']
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import rest_framework.generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList (rest_framework.generics.ListAPIView):
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+
