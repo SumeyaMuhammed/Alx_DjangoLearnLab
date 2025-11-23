ab56a0c56565967ccbaaa1ec422486d5c9016eee
Sumeya Muhammed <sumenaweya@gmail.com>
2025-11-23 09:54:37 +0300

    Authentication and Permissions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ import rest_framework.generics
 import rest_framework.viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAdminUser
 
 class BookList (rest_framework.generics.ListAPIView):
   queryset = Book.objects.all()
@@ -11,3 +12,6 @@ class BookList (rest_framework.generics.ListAPIView):
 class BookViewSet(rest_framework.viewsets.ModelViewSet):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
+  
+  # Only admins can create/update/delete, but everyone authenticated can read
+  permission_classes = [IsAdminUser]
