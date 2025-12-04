647203a024a63a438c160989a523c2ff65fbe967
Sumeya Muhammed <sumenaweya@gmail.com>
2025-12-04 04:57:17 +0300

    Implementing the Blog's User Authentication System

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include
+from django.views.generic import TemplateView
+from .views import SignUpView
+
+urlpatterns = [
+    # Custom login page using built-in LoginView
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+
+    # Logout page using built-in LogoutView
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    # User registration page using the SignUpView
+    path("register/", SignUpView.as_view(), name="register"),
+
+    # Simple template view for profile page after login
+    path('profile/', TemplateView.as_view(template_name='accounts/profile.html')),
+]
