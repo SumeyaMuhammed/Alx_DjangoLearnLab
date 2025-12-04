f1eb0c09d765658785bd7bedd3edd3f3f6dcec6c
Sumeya Muhammed <sumenaweya@gmail.com>
2025-12-04 04:57:17 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+# View for user registration
+class SignUpView(CreateView):
+    form_class = UserCreationForm  # Form used for creating a new user
+    success_url = reverse_lazy('login')  # Redirect URL after successful signup
+    template_name = 'registration/signup.html'  # Template for the signup page
+
+
