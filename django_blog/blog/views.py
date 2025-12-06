ca8968a943212843e009d6cc0c426eedfa4de482
Sumeya Muhammed <sumenaweya@gmail.com>
2025-12-06 20:03:19 +0300

    function-based view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
 
 # View for user registration
-class SignUpView(CreateView):
-    form_class = UserCreationForm  # Form used for creating a new user
-    success_url = reverse_lazy('login')  # Redirect URL after successful signup
-    template_name = 'registration/signup.html'  # Template for the signup page
+def signup(request):
+    if request.method == "POST":       
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()                
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/signup.html", {"form": form})
 
 
