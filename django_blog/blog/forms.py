f47a014878cdacee0aae8021d200342c6ec1a7b9
Sumeya Muhammed <sumenaweya@gmail.com>
2025-12-07 20:27:56 +0300

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content', '').strip()
+        if not content:
+            raise forms.ValidationError("Comment can't be empty.")
+        return content
