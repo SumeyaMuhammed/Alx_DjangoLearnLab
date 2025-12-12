616c7d8b36673519365558f58739f06c8fba191c
Sumeya Muhammed <sumenaweya@gmail.com>
2025-12-13 02:04:38 +0300

    add verb field

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -7,6 +7,7 @@ User = settings.AUTH_USER_MODEL
 class Notification(models.Model):
   recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifiactions')
   actor = models.ForeignKey(User, on_delete=models.CASCADE)
+  verb = models.CharField(max_length=255)
   target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
   target_object_id = models.PositiveIntegerField(null=True, blank=True)
   target = GenericForeignKey('target_content_type', 'target_object_id')
